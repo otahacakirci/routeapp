7000462f9140ea84a7500cfaddb14d460b5f6757
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-24 16:54:24 +0300

    feat: add route card model

---
+++ routeapp/src/main/java/com/osmano/routeapp/route/RouteCard.java
@@ -0,0 +1,36 @@
+package com.osmano.routeapp.route;
+
+import java.util.List;
+
+public class RouteCard {
+
+    private String title;
+
+    private List<String> places;
+
+    public RouteCard(String title) {
+    }
+
+    public RouteCard(String title, List<String> places) {
+        this.title = title;
+        this.places = places;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<String> places) {
+        this.places = places;
+    }
+
+
+}
