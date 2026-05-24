7b5b93648359f152298cc3c67f2f2f1159a3ff4c
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-24 16:48:02 +0300

    chore: initialize Spring Boot project

---
+++ routeapp/src/test/java/com/osmano/routeapp/RouteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmano.routeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
