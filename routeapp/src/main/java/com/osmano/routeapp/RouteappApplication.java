1e7ecb4660f35c42395077d5d44ebd9e8b11491a
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-24 17:35:33 +0300

    chore: add startup runner for route card flow

--- routeapp/src/main/java/com/osmano/routeapp/RouteappApplication.java
+++ routeapp/src/main/java/com/osmano/routeapp/RouteappApplication.java
@@ -1,13 +1,45 @@
 package com.osmano.routeapp;
 
+import com.osmano.routeapp.route.RouteCard;
+import com.osmano.routeapp.route.RouteCardService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class RouteappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RouteappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(RouteCardService routeCardService) {
+		return args -> {
+			RouteCard validRouteCard = new RouteCard(
+					"Kampüs Sonrası Mini Rota",
+					List.of("Kahveci", "Kütüphane", "Park")
+			);
+
+			RouteCard createdRouteCard = routeCardService.createRouteCard(validRouteCard);
+
+			System.out.println("Oluşturulan rota kartı: " + createdRouteCard.getTitle());
+			System.out.println("Mekanlar: " + createdRouteCard.getPlaces());
+
+			RouteCard invalidRouteCard = new RouteCard(
+					"Bozuk Rota",
+					List.of("Kahveci")
+			);
+
+			try {
+				routeCardService.createRouteCard(invalidRouteCard);
+			} catch (IllegalArgumentException exception) {
+				System.out.println("Validation hatası: " + exception.getMessage());
+			}
+		};
 	}
 
 }
