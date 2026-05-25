38659f7b176910ac362a4beedbfe3e62bf5874db
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-26 00:30:05 +0300

    refactor: move startup demo to dedicated runner

--- routeapp/src/main/java/com/osmano/routeapp/RouteappApplication.java
+++ routeapp/src/main/java/com/osmano/routeapp/RouteappApplication.java
@@ -15,31 +15,4 @@ public class RouteappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RouteappApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner run(RouteCardService routeCardService) {
-		return args -> {
-			RouteCard validRouteCard = new RouteCard(
-					"Kampüs Sonrası Mini Rota",
-					List.of("Kahveci", "Kütüphane", "Park")
-			);
-
-			RouteCard createdRouteCard = routeCardService.createRouteCard(validRouteCard);
-
-			System.out.println("Created route card: " + createdRouteCard.getTitle());
-			System.out.println("Places: " + createdRouteCard.getPlaces());
-
-			RouteCard invalidRouteCard = new RouteCard(
-					"Bozuk Rota",
-					List.of("Kahveci")
-			);
-
-			try {
-				routeCardService.createRouteCard(invalidRouteCard);
-			} catch (IllegalArgumentException exception) {
-				System.out.println("Validation error: " + exception.getMessage());
-			}
-		};
-	}
-
 }
---
+++ routeapp/src/main/java/com/osmano/routeapp/config/AppStartupRunner.java
@@ -0,0 +1,46 @@
+package com.osmano.routeapp.config;
+
+import com.osmano.routeapp.route.RouteCard;
+import com.osmano.routeapp.route.RouteCardService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AppStartupRunner implements CommandLineRunner {
+
+    private final RouteCardService routeCardService;
+
+    public AppStartupRunner(RouteCardService routeCardService) {
+        this.routeCardService = routeCardService;
+    }
+
+
+    @Override
+    public void run(String ... args) throws Exception  {
+        RouteCard validRouteCard = new RouteCard(
+                "Kampüs Sonrası Mini Rota",
+                List.of("Kahveci", "Kütüphane", "Park")
+        );
+
+        RouteCard createdRouteCard = routeCardService.createRouteCard(validRouteCard);
+
+        System.out.println("Created route card: " + createdRouteCard.getTitle());
+        System.out.println("Places: " + createdRouteCard.getPlaces());
+
+        RouteCard invalidRouteCard = new RouteCard(
+                "Bozuk Rota",
+                List.of("Kahveci")
+        );
+
+        try {
+            routeCardService.createRouteCard(invalidRouteCard);
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Validation error: " + exception.getMessage());
+        }
+    }
+
+
+
+}
