b484ab4e343b414c2fbc7d264dd98381c6f67eca
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-31 13:29:50 +0300

    refactor: remove unused repository import

--- routeapp/src/main/java/com/osmano/routeapp/route/RouteCardRepository.java
+++ routeapp/src/main/java/com/osmano/routeapp/route/RouteCardRepository.java
@@ -1,7 +1,6 @@
 package com.osmano.routeapp.route;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface RouteCardRepository extends JpaRepository<RouteCard, Long> {
 }
